cb47de270906163c99fe62445d23ad23deac7507
Vrajp610 <vrajptl0610@gmail.com>
2021-11-06 23:45:11 -0400

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+/*************************************************************************
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ *  @author: Vraj Patel, vpp36@scarletmail.rutgers.edu, vpp36
+ *
+ *  Prints "Hello, World". By tradition, this is everyone's first program.
+ *
+ *  % java HelloWorld
+ *  Hello, World
+ *
+ *************************************************************************/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        
+	// WRITE YOUR CODE HERE
+    }
+
+}
